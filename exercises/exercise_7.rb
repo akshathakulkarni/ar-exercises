bf54e47cea2192c860b49c03f6584cc3836f81da
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-01-08 00:00:50 -0500

    Add validations for both Store and Employee models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { in: 40..200 }
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than: 0}
+  validate :one_of_mens_or_womens_apparel
+  def one_of_mens_or_womens_apparel
+    if mens_apparel == false || womens_apparel == false
+      errors.add(:store, "must carry at least one of the mens or womens apparel")
+    end
+  end
+end
+
+puts 'Enter a store name'
+store_name = gets.chomp
+
+@new_store = Store.create(name: store_name)
+puts @new_store.errors.full_messages
