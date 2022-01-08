d0836cd7ec6ba41b650806cfdc4b116bec5d7d20
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-01-08 00:00:14 -0500

    Add one-to-many relationship between Store and Employee class and add some data into employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store 
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Max", last_name: "Karl", hourly_rate: 40)
+@store2.employees.create(first_name: "Samantha", last_name: "Ming", hourly_rate: 40)
+@store2.employees.create(first_name: "Jack", last_name: "Kale", hourly_rate: 50)
