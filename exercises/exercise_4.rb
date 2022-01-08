b7d40e4ace77c7834554ab62021ae56996472667
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-01-07 23:54:32 -0500

    Create 3 more stores and fetch a collection of stores using where class method

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: false)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |each_store|
+  puts "name = #{each_store.name} and annual_revenue = #{each_store.annual_revenue}"
+end
+
+@womens_store = Store.where(mens_apparel: false, womens_apparel: true).where("annual_revenue < ?", 1000000)
+ 
+@womens_store.each do |each_store|
+  puts "Store name = #{each_store.name} and annual_revenue = #{each_store.annual_revenue}"
+end
